378c45c0012dfe566fbf05f041f752373d6a5666
siu <siu.issiki@gmail.com>
2018-06-20 02:50:08 +0900

    edit title

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ const config = {
   ** Headers of the page
   */
   head: {
-    title: 'Custom app with Contentful',
+    title: 'monochromatica',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
