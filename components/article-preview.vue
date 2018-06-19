3c0123275ffa3442aa35ca349a5e51ca061400d8
siu <siu.issiki@gmail.com>
2018-06-19 15:31:30 +0900

    nuxt-linkにimgを含める

--- components/article-preview.vue
+++ components/article-preview.vue
@@ -1,13 +1,15 @@
 <template>
   <article>
-    <img class="thumbnail"
-      :src="post.fields.heroImage.fields.file.url + '?fit=scale&w=350&h=196'"
-      :srcset="`${post.fields.heroImage.fields.file.url}?w=350&h=196&fit=fill 350w, ${post.fields.heroImage.fields.file.url}?w=1000&h=562&fit=fill 1000w, ${post.fields.heroImage.fields.file.url}?w=2000&h=1125&fit=fill 2000w`"
-      sizes="(min-width: 1024px) 400px, 100vw"
-    >
-    <time class="tiny date">{{ ( new Date(post.fields.publishDate)).toDateString() }}</time>
-    <h4><nuxt-link :to="{ name: 'blog-slug', params: { slug: post.fields.slug }}" class="title">{{ post.fields.title }}</nuxt-link></h4>
-    <p>{{ post.fields.description }}</p>
+    <nuxt-link :to="{ name: 'blog-slug', params: { slug: post.fields.slug }}" class="title">
+      <img class="thumbnail"
+        :src="post.fields.heroImage.fields.file.url + '?fit=scale&w=350&h=196'"
+        :srcset="`${post.fields.heroImage.fields.file.url}?w=350&h=196&fit=fill 350w, ${post.fields.heroImage.fields.file.url}?w=1000&h=562&fit=fill 1000w, ${post.fields.heroImage.fields.file.url}?w=2000&h=1125&fit=fill 2000w`"
+        sizes="(min-width: 1024px) 400px, 100vw"
+      >
+      <time class="tiny date">{{ ( new Date(post.fields.publishDate)).toDateString() }}</time>
+      <h4>{{ post.fields.title }}</h4>
+      <p>{{ post.fields.description }}</p>
+    </nuxt-link>
 
     <div class="tags">
       <nuxt-link
