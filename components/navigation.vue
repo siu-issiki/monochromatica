c0e1ea3ee1f91ca031a1d57e2cacc413c3f7ecbf
siu <siu.issiki@gmail.com>
2018-06-21 01:23:38 +0900

    nav title size

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar is-white" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <nuxt-link to="/" class="navbar-item">monochromatica</nuxt-link>
+      <nuxt-link to="/" class="navbar-item is-size-4">monochromatica</nuxt-link>
       <div class="navbar-burger" data-target="navMenu" v-bind:class="{'is-active':isActive}" v-on:click='isActive=!isActive'>
         <span></span>
         <span></span>
