fd0be7adc0d99ba46da96b0ac9a2789b5de2bd6c
siu <siu.issiki@gmail.com>
2018-06-21 14:38:02 +0900

    fix undefined img

--- components/article-preview.vue
+++ components/article-preview.vue
@@ -1,14 +1,14 @@
 <template>
   <article class="box">
     <nuxt-link :to="{ name: 'blog-slug', params: { slug: post.fields.slug }}" class="title">
-      <img class="thumbnail"
-        v-if="post.fields.heroImage.fields.file"
+      <div :id="post.title" >
+      <img class="thumbnail" v-if="'heroImage' in this.post.fields"
         :src="post.fields.heroImage.fields.file.url + '?fit=scale&w=350&h=196'"
         :srcset="`${post.fields.heroImage.fields.file.url}?w=350&h=196&fit=fill 350w, ${post.fields.heroImage.fields.file.url}?w=1000&h=562&fit=fill 1000w, ${post.fields.heroImage.fields.file.url}?w=2000&h=1125&fit=fill 2000w`"
         sizes="(min-width: 1024px) 400px, 100vw"
-      >
-      <time class="tiny date">{{ ( new Date(post.fields.publishDate)).toDateString() }}</time>
+      ></div>
       <h4>{{ post.fields.title }}</h4>
+      <p class="tiny date">{{ ( new Date(post.fields.publishDate)).toDateString() }}</p>
     </nuxt-link>
     <p>{{ post.fields.description }}</p>
 
@@ -26,4 +26,15 @@ export default {
   props: ['post']
 }
 </script>
+
+<style scoped>
+  .date {
+    padding-top: 10px;
+    padding-bottom: 15px;
+  }
+
+  h4 {
+    padding-top: 10px;
+  }
+</style>
 
