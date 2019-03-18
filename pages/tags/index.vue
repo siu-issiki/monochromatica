761fa04e7932383d6e4dffb5425fc18cb7495bfb
Tomoki Tanezaki <siu.issiki@gmail.com>
2019-03-19 01:31:35 +0900

    avoid error

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -15,7 +15,7 @@
           <ul class="column is-4">
             <li>
               <nuxt-link
-                :key="tag"
+                :key="tag.gields.title"
                 :to="{ name: 'tags-tag', params: { tag: tag.fields.title }}"
                 class="tag is-light is-rounded"
               >
